70f774887cec7248d1444988bf8964e6441ddd5a
Igor Andrade <igor.andrade@techne.com.br>
2019-06-04 17:51:36 +0000

    analisar problema

--- src/main/mobileapp/www/js/blockly/fotogrande2.blockly.js
+++ src/main/mobileapp/www/js/blockly/fotogrande2.blockly.js
@@ -7,14 +7,23 @@ window.blockly.js.blockly.Fotogrande2 = window.blockly.js.blockly.Fotogrande2 ||
  * Descreva esta função...
  */
 window.blockly.js.blockly.Fotogrande2.mudaform2 = function(urlFoto) {
- var item, x;
+
   this.cronapi.screen.changeView("#/app/logged/fotogrande2",[ { urlFoto : urlFoto } ]);
+}
+
+/**
+ * Descreva esta função...
+ */
+window.blockly.js.blockly.Fotogrande2.urlFoto = function() {
+ var urlFoto;
+  this.cronapi.screen.notify('success',this.cronapi.screen.getParam('urlFoto'));
+  return this.cronapi.screen.getParam('urlFoto');
 }
 
 /**
  * fotogrande2
  */
 window.blockly.js.blockly.Fotogrande2.Executar = function() {
- var item, urlFoto, x;
+ var urlFoto;
   this.cronapi.screen.changeAttrValue("crn-image-892921", 'src', this.cronapi.screen.getParam('urlFoto'));
 }
